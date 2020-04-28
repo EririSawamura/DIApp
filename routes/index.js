4f2e255c87882b9b8c47dbba996bc8e371fc178d
ERILI <16095841d@connect.polyu.hk>
2020-04-28 16:27:56 +0800

    basic login and register

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
@@ -8,45 +9,77 @@ var connection = mysql.createConnection({
   password: 'password',
   database: 'users'
 });
+
 connection.connect();
 
 router.get('/', function(req, res, next) {
   res.send("Welcome to DIApp.");
 });
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Login' });
+  if(req.cookies['username']){
+    res.redirect('/user');
+  }else{
+    if(req.query.valid){
+      res.render('login', { title: 'Login', hidden:'display:block' });
+    }
+    else{
+      res.render('login', { title: 'Login', hidden:'display:none'});
+    }
+  }
 });
 router.get('/register', function (req, res) {
   res.render('register', { title: 'Register' });
 });
 
 router.post('/login', function (req, res) {
-  login(req.body);
-  res.render('login', { title: 'Login' });
+  login(req, function(result){
+    if(result){
+      console.log(result);
+      res.cookie('username', req.body.username, {expire: 360000 + Date.now()});
+      res.redirect("/user");
+    } else{
+      res.redirect("/login?valid=false");
+    }
+  });
 });
 router.post('/register', function (req, res) {
+  register(req, function(result){
+    if(result){
+      res.redirect("/login");
+    }
+  });
 })
 module.exports = router;
 
-function login(req){
-  console.log(req);
+function login(req, callback){
+  var sql = "SELECT * FROM user WHERE username = ? AND password = ?";
   connection.query(
-    "SELECT * FROM user WHERE username = ? AND password = ?",[
-      req.username,
-      req.pwd],
-    function (err, rows, fields) {
+      sql,[
+      req.body.username,
+      req.body.pwd],
+    function (err, rows) {
       if (err) throw err;
-    
-      if(rows.length == 1){
-        console.log("nice");
+      
+      if(rows.length === 1){
+        return callback(true);
       }
       else{
-        console.log("shit");
+        return callback(false);
       }
     }
   );
 }
 
-function register(){
-
+function register(req, callback){
+  var sql = "INSERT INTO user VALUES (?, ?)";
+  connection.query(
+      sql,[
+      req.body.username,
+      req.body.pwd],
+    function (err, rows) {
+      if (err) throw err;
+      
+      return callback(true);
+    }
+  );
 }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  if(req.cookies['username']){
+    res.render('user', { title: 'User' });
+  }
+  else{
+    res.render('login',{ title:'login' });
+  }
+});
+
+router.post('/', function(req, res, next) {
+  res.clearCookie('username');
+  res.redirect("login");
 });
 
 module.exports = router;
