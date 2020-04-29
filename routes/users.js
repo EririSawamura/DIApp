c856ea69e4ae55879cf62bfe9ea827917581fd85
ERILI <16095841d@connect.polyu.hk>
2020-04-29 18:39:10 +0800

    Upload

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var date = new Date();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'root',
+  password: 'password',
+  database: 'users'
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,15 +16,25 @@ router.get('/', function(req, res, next) {
 
   if(req.cookies['username']){
     msg = genWelmsg(req.cookies['username']);
-    res.render('user', { title: 'User', username: msg});
+    genPwdlist(req.cookies['username'], function(result){
+      res.render('user', { title: 'User', username: msg, result: result});
+    });
   } else{
     res.redirect('/login');
   }
 });
+router.get('/create_key', function(req, res, next) {
+  res.render('create_key', {title: 'Create new key pairs'})
+});
+
 
 router.post('/', function(req, res, next) {
-  res.clearCookie('username');
-  res.redirect("login");
+  if(req.body.logout){
+    res.clearCookie('username');
+    res.redirect("login");
+  } else{
+    res.redirect('/user/create_key');
+  }
 });
 
 function genWelmsg(name){
@@ -30,4 +48,17 @@ function genWelmsg(name){
   }
 }
 
+function genPwdlist(name, callback){
+  var list_sql = "SELECT key_id,  USERNAME FROM key_store WHERE owner = ? ORDER BY LAST_MODIFIED_TIME DESC;";
+  connection.query(
+    list_sql,[
+      name
+    ],
+    function (err, rows) {
+      if (err) throw err;
+    
+      return callback(rows);
+    }
+  );
+}
 module.exports = router;
